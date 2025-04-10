78014cfee6bd7f0ec374b0cc4bd3706e45cc522d
seraprogrammer <programmernazmul1@gmail.com>
2025-04-10 09:12:25 +0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent, ChangeEvent } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 interface EmailTemplate {
